e1be1a15395b78e179a6b0db9e040707a43e8ea2
MaëlieA <maelie@addario.fr>
2025-01-24 11:57:19 +0100

    alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: netlify()
+  integrations: [tailwind(), alpinejs()],
+  adapter: netlify(),
 });
